92d9f70b4d8d132ba208559d9dfb1a1cfcf16045
Samadhan Jagannath Sawant <samadhans@cybage.com>
2022-02-21 18:23:06 +0530

    project files added

---
+++ BLOGSPOT/public/writeblog.js
@@ -0,0 +1,23 @@
+const form = document.querySelector('form');
+
+const createPost = async (e) => {
+  e.preventDefault();
+
+  const doc = {
+    category: form.Category.value,
+    title: form.title.value,
+    AutherName: form.AutherName.value,
+    body: form.body.value,
+    likes: 0,
+  }
+
+  await fetch('http://localhost:3000/posts', {
+    method: 'POST',
+    body: JSON.stringify(doc),
+    headers: { 'Content-Type': 'application/json' }
+  })
+
+  window.location.replace('/index2.html')
+}
+
+form.addEventListener('submit', createPost);
